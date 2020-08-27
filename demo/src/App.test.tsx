85bd8bdeed55911e42345b847816ad0f3f5f6857
Raphael Thomazella <thomazella9@gmail.com>
2020-08-26 22:26:37 -0300

    fix(jest): fix App test

--- demo/src/App.test.tsx
+++ demo/src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+/**
+ * @jest-environment jsdom
+ */
+import React from "react";
+import { render, act } from "@testing-library/react";
+import App from "./App";
+import "@testing-library/jest-dom/extend-expect";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  const { getAllByText } = render(<App />);
+  const linkElement = getAllByText(/Posts/i);
+  expect(linkElement[0]).toBeInTheDocument();
 });
